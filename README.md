ae6c97b6e3c7d9144f42f12336bdae410d8684fe
coderaiser <coderaiser@cloudcmd.io>
2018-08-10 11:56:21 +0300

    feature(binarnia) add

---
+++ README.md
@@ -0,0 +1,83 @@
+# Binarnia [![License][LicenseIMGURL]][LicenseURL] [![NPM version][NPMIMGURL]][NPMURL] [![Dependency Status][DependencyStatusIMGURL]][DependencyStatusURL] [![Build Status][BuildStatusIMGURL]][BuildStatusURL]
+
+Parse binary buffer to json according to schema.
+
+## Install
+
+```
+npm i binarnia
+```
+
+## How to use?
+
+```js
+const binarnia = require('binarnia');
+const schema = [{
+    offset: '0x00',
+    name: "format",
+    length: 1,
+    type: "enum",
+    enum: {
+        '0x22': 'MZ',
+        '0x33': 'PE',
+    }
+}, {
+    offset: '0x01',
+    name: "arch",
+    length: 1,
+    type: "array",
+    array: [
+        'x32',
+        'x64',
+    ]
+}];
+
+const buffer = Buffero.from([0x22, 0x01]);
+
+// endianness = 'LE'
+binarnia(schema, buffer);
+// returns
+{
+    arch: 'x32',
+    format: 'MZ'
+};
+
+// directly pass endianness
+binarnia(schema, 'BE', buffer);
+// returns
+{
+    arch: 'x32',
+    format: 'MZ'
+};
+
+const array = [0x22, 0x01];
+// works with array as well
+binarnia(schema, 'BE', array);
+// returns
+{
+    arch: 'x32',
+    format: 'MZ'
+};
+```
+
+## Environments
+
+In old `node.js` environments that not fully supports `es2015`, `binarnia` could be used with:
+
+```js
+var binarnia = require('binarnia/legacy');
+```
+
+## License
+
+MIT
+
+[NPMIMGURL]:                https://img.shields.io/npm/v/binarnia.svg?style=flat&longCache=true
+[BuildStatusIMGURL]:        https://img.shields.io/travis/coderaiser/binarnia/master.svg?style=flat&longCache=true
+[DependencyStatusIMGURL]:   https://img.shields.io/david/coderaiser/binarnia.svg?style=flat&longCache=true
+[LicenseIMGURL]:            https://img.shields.io/badge/license-MIT-317BF9.svg?style=flat&longCache=true
+[NPMURL]:                   https://npmjs.org/package/binarnia "npm"
+[BuildStatusURL]:           https://travis-ci.org/coderaiser/binarnia  "Build Status"
+[DependencyStatusURL]:      https://david-dm.org/coderaiser/binarnia "Dependency Status"
+[LicenseURL]:               https://tldrlegal.com/license/mit-license "MIT License"
+
---
+++ test/binarnia.js
@@ -0,0 +1,134 @@
+'use strict';
+
+const test = require('tape');
+const binarnia = require('..');
+
+test('binarnia: no args', (t) => {
+    t.throws(binarnia, /schema should be an array!/, 'should throw when no args');
+    t.end();
+});
+
+test('binarnia: no buffer', (t) => {
+    const fn = () => binarnia([]);
+    t.throws(fn, /buffer should be buffer or an array!/, 'should throw when no buffer');
+    t.end();
+});
+
+test('binarnia: value', (t) => {
+    const schema = [{
+        offset: '0x00',
+        name: "format",
+        length: 1,
+        type: "value",
+    }];
+    
+    const buffer = [0x33];
+    
+    const result = binarnia(schema, buffer);
+    const expected = {
+        format: '0x33'
+    };
+    
+    t.deepEqual(result, expected, 'should equal');
+    t.end();
+});
+
+test('binarnia: array', (t) => {
+    const schema = [{
+        offset: '0x00',
+        name: "format",
+        length: 1,
+        type: "array",
+        array: [
+            'MZ',
+            'PE',
+        ]
+    }];
+    
+    const buffer = [0x00];
+    
+    const result = binarnia(schema, buffer);
+    const expected = {
+        format: 'MZ'
+    };
+    
+    t.deepEqual(result, expected, 'should equal');
+    t.end();
+});
+
+test('binarnia: enum', (t) => {
+    const schema = [{
+        offset: '0x00',
+        name: "format",
+        length: 1,
+        type: "enum",
+        enum: {
+            '0x22': 'MZ',
+            '0x33': 'PE',
+        }
+    }];
+    
+    const buffer = [0x22];
+    
+    const result = binarnia(schema, buffer);
+    const expected = {
+        format: 'MZ'
+    };
+    
+    t.deepEqual(result, expected, 'should equal');
+    t.end();
+});
+
+test('binarnia: LE', (t) => {
+    const schema = [{
+        offset: '0x00',
+        name: "format",
+        length: 4,
+        type: "value",
+    }];
+    
+    const buffer = [0x01, 0x02, 0x03, 0x04];
+    
+    const result = binarnia(schema, buffer);
+    const expected = {
+        format: '0x4030201'
+    };
+    
+    t.deepEqual(result, expected, 'should equal');
+    t.end();
+});
+
+test('binarnia: BE', (t) => {
+    const schema = [{
+        offset: '0x00',
+        name: "format",
+        length: 4,
+        type: "value",
+    }];
+    
+    const buffer = [0x01, 0x02, 0x03, 0x04];
+    
+    const result = binarnia(schema, 'BE', buffer);
+    const expected = {
+        format: '0x1020304'
+    };
+    
+    t.deepEqual(result, expected, 'should equal');
+    t.end();
+});
+
+test('binarnia: not defined', (t) => {
+    const schema = [{
+        offset: '0x00',
+        name: "format",
+        length: 4,
+        type: "super",
+    }];
+    
+    const buffer = [0x01, 0x02, 0x03, 0x04];
+    
+    const fn = () => binarnia(schema, 'BE', buffer);
+    t.throws(fn, /0x00: format: behaviour of type "super" is not defined/, 'should throw');
+    
+    t.end();
+});
